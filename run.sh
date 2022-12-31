089708f656b30c7bbb2cb46c3e2c113014f0f645
urbao <E24094198@gs.ncku.edu.tw>
2022-12-31 10:46:00 +0800

    finished colored output func

--- run.sh
+++ run.sh
@@ -1,3 +1,29 @@
 #!/bin/bash
-cd /home/eason/Desktop/money\ tracker
-./a.out
+
+# print fnction with color and newline/sameline options
+# nl: NewLine/ nnl: No NewLine
+function print(){
+	local c_code=0
+	# color c_code chck
+	if [ "$1" == "red" ]; then c_code="31m" 
+	elif [ "$1" == "yellow" ]; then c_code="33m" 
+	elif [ "$1" == "green" ]; then c_code="32m" 
+	elif [ "$1" == "cyan" ]; then c_code="36m" 
+	elif [ "$1" == "purple" ]; then c_code="35m"
+	#white output(Default)
+	else c_code="37m" 
+	fi
+	# combine c_code with printed line(string combination)
+	c_code+="$2"
+	# new line or not
+	# 'echo -e' enables color hex code identification 
+	if [ "$3" == "nl" ]; then echo -e  "\e[1;${c_code}\e[0m"
+	# '-n' parameter means no newline
+	else echo -n -e "\e[1;${c_code} \e[0m"
+	fi
+	return 
+}
+
+print "yellow" "test line" "nl"
+print "cyan" "urbao:" "nnl"
+sleep 5
